9914c04eade59a2419607c1ef36e4867ecb7d329
zharq03 <azhar.rizqullah135@gmail.com>
2025-05-05 18:41:31 +0700

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/", 
+  build: {
+    outDir: "dist",
+    publicDir: false, 
+  },
+  server: {
+    open: true,
+  },
+  resolve: {
+    alias: {
+      "./runtimeConfig": "./runtimeConfig.browser", // mencegah error MIME 
+    },
+  },
 });
